f82dd3795d54cae273db27e3f1a8248b4bc15c10
hartogss <sigihart@bluewin.ch>
2020-10-21 18:20:49 +0200

    Cleanup

--- Langstat.cpp
+++ Langstat.cpp
@@ -1,29 +1,26 @@
-// helpful: https://github.com/peter-can-talk/cppnow-2017/blob/master/code/mccabe/mccabe.cpp
-
-// clang includes
+// llvm & clang includes
 #include "clang/Frontend/FrontendAction.h"
 #include "clang/Tooling/Tooling.h"
 #include "clang/Tooling/CommonOptionsParser.h"
 #include "clang/ASTMatchers/ASTMatchFinder.h"
-
 // standard includes
 #include <iostream>
-
-//
+// custom includes
 #include "Analysis.h"
 
-//
+// namespaces
 using namespace clang; // CompilerInstance, ASTFrontendAction, ASTConsumer
-using namespace clang::ast_matchers;
+using namespace clang::ast_matchers; // StatementMatcher, actual AST matchers like forStmt etc.
+using namespace clang::tooling; // CommonOptionsParser
 
+
+//-----------------------------------------------------------------------------
 
 // Class called on a match
 class MatchHandler : public ast_matchers::MatchFinder::MatchCallback {
 public:
-    MatchHandler () {
-        Analysis an;
-        An = an;
-    }
+    // ctor
+    MatchHandler() = default;
     // Gets called on a match by MatchFinder
     void run(const ast_matchers::MatchFinder::MatchResult& Result){
         std::cout << "Found match in AST" << std::endl;
@@ -34,19 +31,18 @@ public:
     Analysis An;
 };
 
+//-----------------------------------------------------------------------------
 
-// Consumes, i.e. does computations on the AST
+
+// Consumes the AST, i.e. does computations on the AST
 class Consumer : public ASTConsumer {
 public:
-    // Constructor, how is finder & handler constructed?
+    // ctor
     Consumer(Analysis An){
         std::cout << "Consumer() called" << std::endl;
-        using namespace clang::ast_matchers; //what does 'using' without 'namespace' do? -> sort of typedef
-        // if(forstmtflag){
-            std::cout << "Adding matchers to finder" << std::endl;
-            Handler.An = An;
-            Finder.addMatcher(An.getMatcher(), &Handler);
-        // }
+        std::cout << "Adding matchers to finder" << std::endl;
+        Handler.An = An;
+        Finder.addMatcher(An.getMatcher(), &Handler);
     }
     // Called when AST for TU is ready/has been parsed
     void HandleTranslationUnit(clang::ASTContext& Context){
@@ -57,37 +53,42 @@ private:
     // Brings together all Matchers and callbacks when something matches
     ast_matchers::MatchFinder Finder;
     MatchHandler Handler;
+    // how are these contructed?
 };
 
+//-----------------------------------------------------------------------------
 
-// Responsible for steering when what is executed, LibTooling
+
+// Responsible for steering when what is executed
 class Action : public ASTFrontendAction {
 public:
     // Constructor
-    // Action(){
-    //     std::cout << "Creating AST action" << std::endl;
-    // }
+    Action(){
+        std::cout << "Creating AST action" << std::endl;
+    }
+    // ctor
     Action(Analysis An) : An(An){
         std::cout << "Creating AST action with arg" << std::endl;
-    }
-    // Called after frontend is initialized, but before per-file processing
-    // why virtual?
-    virtual std::unique_ptr<clang::ASTConsumer> CreateASTConsumer(
-        CompilerInstance &CI, llvm::StringRef InFile){
-            std::cout << "CreateASTConsumer() called" << std::endl;
-            return std::make_unique<Consumer>(An);
     }
     // Called at start of processing a single input
     bool BeginSourceFileAction(CompilerInstance& CI) {
         std::cout << "Starting to process file" << std::endl;
+        std::cout << getCurrentFile().data() << std::endl;
         return true;
+    }
+    // Called after frontend is initialized, but before per-file processing
+    std::unique_ptr<clang::ASTConsumer> CreateASTConsumer(
+        CompilerInstance &CI, llvm::StringRef InFile){
+            std::cout << "CreateASTConsumer() called" << std::endl;
+            return std::make_unique<Consumer>(An);
     }
 private:
     Analysis An;
 };
 
+//-----------------------------------------------------------------------------
+// CL options
 
-using namespace clang::tooling;
 // Options in CLI specific/nongeneric to clang-stat
 llvm::cl::OptionCategory ClangStatCategory("clang-stat options");
 llvm::cl::extrahelp CommonHelp(CommonOptionsParser::HelpMessage);
@@ -104,43 +105,47 @@ llvm::cl::opt<bool> WSOption(
     llvm::cl::cat(ClangStatCategory)
 );
 
+//-----------------------------------------------------------------------------
+// Should move to separate file
 
-// Responsible for building Actions
-// class Factory : public clang::tooling::FrontendActionFactory {
-// public:
-//     // 'ctor'
-//     std::unique_ptr<FrontendAction> create() override {
-//         return std::make_unique<Action>();
-//     }
-// };
+// Add analysis
+std::string Name = "fs";
+StatementMatcher Matcher = forStmt().bind(Name);
+auto Runner = [](auto Node) {
+    std::cout << "fs matcher runner called" << std::endl;
+    Node->dump();
+};
+Analysis forstmt(Name, Matcher, Runner);
 
+//-----------------------------------------------------------------------------
 
-// const: pass in this case by pointer (could be reference too), but
-// restrict callee from being able to change passed data
-// avoids (maybe slow) copy
+// Responsible for building Actions to give ToolAction to ClangTool
+class Factory : public clang::tooling::FrontendActionFactory {
+public:
+    // 'ctor'
+    std::unique_ptr<FrontendAction> create() {
+        std::cout << "create() called" << std::endl;
+        return std::make_unique<Action>(forstmt);
+    }
+};
+
+//-----------------------------------------------------------------------------
+
 int main(int argc, const char** argv){
-    using namespace clang::tooling;
-    using namespace clang::ast_matchers;
-
 
     // parses all options that command-line tools have in common
     CommonOptionsParser Parser(argc, argv, ClangStatCategory);
 
+    // Print all file name & locations
+    for (auto SourceFilePath : Parser.getSourcePathList()){
+        std::cout << SourceFilePath << std::endl;
+    }
+
     ClangTool Tool(Parser.getCompilations(), Parser.getSourcePathList());
 
-    // Add analysis
-    std::string Name = "fs";
-    StatementMatcher Matcher = whileStmt().bind(Name);
-    auto Runner = [](auto Node) {
-        std::cout << "fs matcher runner called" << std::endl;
-        Node->dump();
-    };
-    Analysis forstmt(Name, Matcher, Runner);
-
-
-    // util to run frontendaction over file
-    // ClangTool CT
-    clang::tooling::runToolOnCode(std::make_unique<Action>(forstmt), argv[1]); //uses string of code from command line
-    // return Tool.run(new Factory()); // input file, .cpp or .ast
+    // Runs on string of code specified in command line
+    // clang::tooling::runToolOnCode(std::make_unique<Action>(forstmt), argv[1]);
+    // Runs over all files specified in command line
+    Tool.run(new Factory());
     return 0;
 }
