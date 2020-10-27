fba453b96eb1a70c80a8417a74def0e31be6f125
hartogss <sigihart@bluewin.ch>
2020-10-27 10:30:13 +0100

    Fixing git error

--- Analysis.cpp
+++ Analysis.cpp
@@ -1,44 +1,20 @@
-#include "clang/Frontend/FrontendAction.h"
 #include "Analysis.h"
-#include "clang/ASTMatchers/ASTMatchFinder.h"
+
+using namespace clang::ast_matchers;
 
 
-#include <iostream>
-
-using namespace clang; // CompilerInstance, ASTFrontendAction, ASTConsumer, ASTContext
-using namespace clang::ast_matchers; // StatementMatcher, actual AST matchers like forStmt etc.
-using namespace clang::tooling; // CommonOptionsParser
-
-//-----------------------------------------------------------------------------
-
-void MatchHandler::run(const MatchFinder::MatchResult& Result) {
-    std::cout << "default run" << std::endl;
+// Definitions for class 'Analysis'
+// Empty analysis ctor
+Analysis::Analysis() : BaseMatcher(allOf(hasAncestor(forStmt()), unless(hasAncestor(forStmt())))){
 }
-
-//-----------------------------------------------------------------------------
-
-// void Consumer::HandleTranslationUnit(ASTContext& Context) {
-    // std::cout << "default handleTU" << std::endl;
-// }
-
-//-----------------------------------------------------------------------------
-
-// bool Action::BeginSourceFileAction(CompilerInstance& CI) {
-//     std::cout << "Starting to process file"
-//     << getCurrentFile().data() << std::endl;
-//     return true;
-// }
-// std::unique_ptr<clang::ASTConsumer>
-// Action::CreateASTConsumer(
-//     clang::CompilerInstance &CI, llvm::StringRef InFile){
-//         std::cout << "CreateASTConsumer() called" << std::endl;
-//         return std::make_unique<Consumer>();
-// }
-
-//-----------------------------------------------------------------------------
-
-// std::unique_ptr<FrontendAction>
-// Factory::create() {
-//     std::cout << "Factory created" << std::endl;
-//     return std::make_unique<Action>();
-// }
+// ctor
+Analysis::Analysis(std::string Name, StatementMatcher BaseMatcher,
+    std::function<void(const MatchFinder::MatchResult&)> Runner) :
+    Name(Name),
+    BaseMatcher(BaseMatcher),
+    Runner(Runner) {
+}
+//
+StatementMatcher Analysis::getBaseMatcher(){
+    return BaseMatcher;
+}
--- Analysis.h
+++ Analysis.h
@@ -3,55 +3,25 @@
 
 #include "clang/ASTMatchers/ASTMatchers.h"
 #include "clang/ASTMatchers/ASTMatchFinder.h"
-#include "clang/Tooling/Tooling.h"
 
-
-//-----------------------------------------------------------------------------
-
-// Class called on a match
-class MatchHandler : public clang::ast_matchers::MatchFinder::MatchCallback {
+class Analysis {
 public:
-    // Gets called on a match by MatchFinder
-    virtual void run(const clang::ast_matchers::MatchFinder::MatchResult& Result);
+    // Empty analysis ctor
+    Analysis();
+    // ctor
+    Analysis(std::string name,
+            clang::ast_matchers::StatementMatcher BaseMatcher,
+            std::function<void(const clang::ast_matchers::MatchFinder::MatchResult&)> Runner);
+    //
+    clang::ast_matchers::StatementMatcher getBaseMatcher();
+    //
+    std::string Name;
+    // should become more general for example to support analyses that rely on
+    // something else than matchers -> maybe with inheritance/templates
+    clang::ast_matchers::StatementMatcher BaseMatcher;
+    // Runner: defines what happens when matchcallback is called on a match
+    // template <typename T> std::function<void(T)> Runner;
+    std::function<void(const clang::ast_matchers::MatchFinder::MatchResult&)> Runner;
 };
-
-//-----------------------------------------------------------------------------
-
-
-// Consumes the AST, i.e. does computations on the AST
-// class Consumer : public clang::ASTConsumer {
-// public:
-//     // ctor
-//     Consumer();
-//     // Called when AST for TU is ready/has been parsed
-    // virtual void HandleTranslationUnit(clang::ASTContext& Context)=0;
-// private:
-//     // Brings together all Matchers and callbacks when something matches
-    // clang::ast_matchers::MatchFinder Finder;
-    // MatchHandler Handler;
-//     // how are these contructed?
-// };
-
-//-----------------------------------------------------------------------------
-
-// Responsible for steering when what is executed
-// class Action : public clang::ASTFrontendAction {
-// public:
-    // // Called at start of processing a single input
-    // virtual bool BeginSourceFileAction(clang::CompilerInstance& CI) override;
-    // Called after frontend is initialized, but before per-file processing
-    // virtual std::unique_ptr<clang::ASTConsumer>
-    // CreateASTConsumer(clang::CompilerInstance &CI, llvm::StringRef InFile) override;
-// };
-
-//-----------------------------------------------------------------------------
-
-// Responsible for building Actions to give ToolAction to ClangTool
-// class Factory : public clang::tooling::FrontendActionFactory {
-// public:
-//     // 'ctor'
-//     std::unique_ptr<clang::FrontendAction> create();
-// };
-
 
 #endif /* ANALYSIS_H */
