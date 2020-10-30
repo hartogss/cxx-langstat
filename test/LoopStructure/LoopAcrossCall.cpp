827a67a2fd1fa123c338cacd028ec90b8f7b0651
hartogss <sigihart@bluewin.ch>
2020-10-30 17:01:19 +0100

    Added more test code to LoopAcrossCall

--- test/LoopStructure/LoopAcrossCall.cpp
+++ test/LoopStructure/LoopAcrossCall.cpp
@@ -1,10 +1,40 @@
-void func(){
+void callee1(){ //functionDecl(hasBody(compoundStmt(hasDescendant(forStmt()))))
     for(;;){
     }
 }
 
-int main(int argc, char** argv){
-    for(;;){
-        func();
+void callee2(){ //functionDecl(hasBody(compoundStmt(hasDescendant(forStmt()))))
+    if(true){
+        for(;;){
+        }
     }
+}
+
+void callee3(){ //functionDecl(hasBody(compoundStmt(hasDescendant(forStmt()))))
+    if(true){
+
+    }
+    for(;;){
+    }
+}
+
+void callee4(){ //functionDecl(hasBody(compoundStmt(hasDescendant(forStmt()))))
+    if(true){
+
+    }
+    callee1();
+    for(;;){
+    }
+}
+
+
+void caller(){ //functionDecl(hasBody(compoundStmt(hasDescendant(forStmt()))))
+    for(;;){        // forStmt(hasDescendant(callExpr()))
+        callee1();
+    }
+}
+
+
+
+int main(int argc, char** argv){
 }
