e3766187914599fa04420d0b576bd58634b9a161
hartogss <sigihart@bluewin.ch>
2020-11-14 11:33:51 +0100

    Added templates for future analyses

---
+++ templates/template.h
@@ -0,0 +1,26 @@
+#ifndef TEMPLATE_H
+#define TEMPLATE_H
+
+#include "cxx-langstat/Analysis.h"
+
+//-----------------------------------------------------------------------------
+
+class Template : public Analysis {
+public:
+    Template(clang::tooling::ClangTool Tool);
+    // step 0: createMatcher(s)/getMatchers? idea is to have library of often-used
+    // matchers or maybe even some datastructure of important matches itself
+    // step 1: extraction
+    void extract() override;
+    //step 2: compute stats
+    void analyze() override;
+    //step 3: visualization (for later)
+    // combine
+    void run() override;
+};
+
+void runclangtidy();
+
+//-----------------------------------------------------------------------------
+
+#endif /* TEMPLATE_H */
