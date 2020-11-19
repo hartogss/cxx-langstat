83ad248ab97811bde6d12b9b3760162fe756584e
hartogss <sigihart@bluewin.ch>
2020-11-19 16:33:25 +0100

    Small fixes cmakelists and analysis template

--- templates/template.h
+++ templates/template.h
@@ -8,18 +8,15 @@
 class Template : public Analysis {
 public:
     Template(clang::tooling::ClangTool Tool);
-    // step 0: createMatcher(s)/getMatchers? idea is to have library of often-used
-    // matchers or maybe even some datastructure of important matches itself
     // step 1: extraction
     void extract() override;
     //step 2: compute stats
     void analyze() override;
     //step 3: visualization (for later)
+    
     // combine
     void run() override;
 };
-
-void runclangtidy();
 
 //-----------------------------------------------------------------------------
 
