6b1c903b9df0e6a14b001a9357254c3041df85d0
hartogss <sigihart@bluewin.ch>
2020-11-20 16:41:34 +0100

    Legacy BaseExtractor::extract method templatized for different node extraction & moved to header file

--- lib/BaseExtractor.cpp
+++ lib/BaseExtractor.cpp
@@ -14,21 +14,5 @@ using namespace clang::ast_matchers; // StatementMatcher, actual AST matchers li
 BaseExtractor::BaseExtractor(clang::ASTContext& Context) : Context(Context){
     std::cout<<"BaseExtractor ctor"<<std::endl;
 }
-Matches<Stmt> BaseExtractor::extract(std::string id,
-    StatementMatcher Matcher){
-        MatchingExtractor<Stmt, const char*> extr(id);
-        MatchFinder Finder;
-        Finder.addMatcher(Matcher, &extr);
-        Finder.matchAST(Context);
-        return extr.matches[0];
-}
-Matches<Decl> BaseExtractor::extract(std::string id,
-    DeclarationMatcher Matcher){
-        MatchingExtractor<Decl, const char*> extr(id);
-        MatchFinder Finder;
-        Finder.addMatcher(Matcher, &extr);
-        Finder.matchAST(Context);
-        return extr.matches[0];
-}
 
 //-----------------------------------------------------------------------------
