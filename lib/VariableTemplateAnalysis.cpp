bfe1d0a7976c3239cbe12b7f0ace75725630cb84
hartogss <sigihart@bluewin.ch>
2020-11-25 17:04:10 +0100

    VTA: hugely improved matcher looking for constexpr func. temp. decls & added more code to test it

--- lib/VariableTemplateAnalysis.cpp
+++ lib/VariableTemplateAnalysis.cpp
@@ -7,6 +7,23 @@ using namespace clang;
 using namespace clang::ast_matchers;
 
 //-----------------------------------------------------------------------------
+
+std::string getDeclName(Match<clang::Decl> node){
+    if(auto n = dyn_cast<clang::NamedDecl>(node.node)){
+        return n->getNameAsString();
+    } else {
+        std::cout << "Decl @ " << node.location << "cannot be resolved" << std::endl;
+        return "INVALID";
+    }
+}
+
+void printStatistics(std::string text, Matches<clang::Decl> matches){
+    std::cout << "\033[33m" << text << ":\033[0m " << matches.size() << "\n";
+    for(auto m : matches)
+        std::cout << getDeclName(m) << " @ " << m.location << std::endl;
+}
+
+//-----------------------------------------------------------------------------
 // Question: did programmers abandon classes with static fields and constexpr
 // functions returning the value in favor of variable templates, which were
 // introduced in C++14?
@@ -16,26 +33,50 @@ VariableTemplateAnalysis::VariableTemplateAnalysis(ASTContext& Context) :
 }
 void VariableTemplateAnalysis::extract(){
 
-    DeclarationMatcher VariableTemplate = varDecl(
-        has(cxxUnresolvedConstructExpr())
-    ).bind("variabletemplate");
-
     DeclarationMatcher ClassWithStaticVar =
     cxxRecordDecl(has(varDecl(isStaticStorageClass())))
     .bind("classwithstaticvar");
 
     DeclarationMatcher ConstexprFunction = functionTemplateDecl(
+        isExpansionInMainFile(),
+        // FunctionDecl that is Constexpr
         has(functionDecl(
             isConstexpr(),
-            has(varDecl()),
-            unless(has(decl(unless(varDecl())))))))
+            // whose body contains
+            has(compoundStmt(
+                // at least a variable declaration and a return that returns it
+                has(declStmt(has(varDecl().bind("datadecl")))),
+                has(returnStmt(has(
+                    declRefExpr(to(varDecl(equalsBoundNode("datadecl"))))))),
+                // and does not contain anything else. Sufficient to filter for
+                // statement since function body contains statements only. Decl
+                // inside of body handled via a declStmt - which is a Stmt too.
+                unless(has(stmt(
+                    unless(anyOf(
+                        declStmt(has(varDecl())),
+                        returnStmt()))))))))))
     .bind("constexprfunction");
+
+    DeclarationMatcher VariableTemplate = varDecl(
+        has(cxxUnresolvedConstructExpr())
+    ).bind("variabletemplate");
+
+    auto ClassWithStaticVarDecls = Extr.extract("classwithstaticvar", ClassWithStaticVar);
+    auto ConstexprFunctionDecls = Extr.extract("constexprfunction", ConstexprFunction);
+    auto VariableTemplateDecls = Extr.extract("variabletemplate", VariableTemplate);
+
+    printStatistics("Class templates with static data", ClassWithStaticVarDecls);
+    printStatistics("Constexpr function templates", ConstexprFunctionDecls);
+    printStatistics("Variable templates", VariableTemplateDecls);
+
 
 
 }
 void VariableTemplateAnalysis::analyze(){
 }
 void VariableTemplateAnalysis::run(){
+    extract();
+    analyze();
 }
 
 //-----------------------------------------------------------------------------
--- test/VariableTemplate.cpp
+++ test/VariableTemplate.cpp
@@ -1,7 +1,9 @@
-#include <iostream>
+// #include <iostream>
+
+//-----------------------------------------------------------------------------
 
 // Old way Nr. 1:
-// 'data' should be static because everybody using this
+// 'data' should be static because everybody using this should see the same?
 template<typename T>
 class s {
 public:
@@ -12,16 +14,74 @@ int s<int>::data = 42;
 // template<>
 // int s3<double>::data = 41.0;
 
+//-----------------------------------------------------------------------------
+
 // Old way Nr. 2:
+// Constexpr function template (CFT)
 template<typename T>
-constexpr T func(){
+constexpr T f1(){
     T data = 3;
     return data;
 }
+// Not CFT as not constexpr
+template<typename T>
+T f2(){
+    T data = 3;
+    return data;
+}
+// Not CFT as not templated
+constexpr int f3(){
+    int data = 3;
+    return data;
+}
+// Probably shouldn't be CFT since it does not return the data it defines
+template<typename T>
+constexpr T f4(){
+    T data = 3;
+    return 0;
+}
+// Shouldn't be CFT, doesn't define anything
+template<typename T>
+constexpr T f5(){
+    return 0;
+}
+// Not CFT, doesn't return
+template<typename T>
+constexpr T f6(){
+    T data = 3;
+}
+// Not CFT, has function call
+template<typename T>
+constexpr T f7(){
+    T data;
+    f1<T>();
+    return data;
+}
+// Not CFT, returns something different that data (irrelevant that it's a call)
+template<typename T>
+constexpr T f8(){
+    T data;
+    return f1<T>();
+}
+// Not CFT, contain declStmt that contains typedef
+template<typename T>
+constexpr T f9(){
+    T data;
+    typedef T T_prime;
+    return data;
+}
+// Debatable.
+template<typename T>
+constexpr T f10(){
+    T data;
+    T data_prime;
+    return data;
+}
+
 
 // Now: variable templates (since C++14)
 template<typename T>
-constexpr T pi = T(3);
+constexpr T pi = T(3.14); // what does T() do?
 
 class s2 {
     template<typename T>
@@ -30,15 +90,19 @@ class s2 {
     //     template<typename T>
     //     static constexpr T pi2 = T(3);
     // }
+    // constexpr int t = 3;
 };
 
 int main(int argc, char** argv){
     s<int> s1;
     s1.data = 3;
     s<int> s2;
-    std::cout << s<int>::data << std::endl;
-    std::cout << s<double>::data << std::endl;
-    std::cout << s1.data << std::endl;
-    std::cout << s2.data << std::endl;
+    pi<double>;
+    pi<int>;
+    int x = 3;
+    // std::cout << s<int>::data << std::endl;
+    // std::cout << s<double>::data << std::endl;
+    // std::cout << s1.data << std::endl;
+    // std::cout << s2.data << std::endl;
 
 }
