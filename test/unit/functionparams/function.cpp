c13161263e81c3d6091ecf4cd911d44fecdc235f
hartogss <sigihart@bluewin.ch>
2021-01-05 17:20:00 +0100

    added test file that should've been in prev commit

---
+++ test/unit/functionparams/function.cpp
@@ -0,0 +1,34 @@
+// RUN: clang++ %s -emit-ast -o %t1.ast
+// RUN: %S/../../../build/cxx-langstat --analyses=msa --store %t1.ast --
+// RUN: diff %t1.ast.json %s.json
+
+// Test to see if correctly for each kind of function (non-template) declaration
+// parameter kind the right functions are found and all parmeters are listed
+// correctly.
+
+class C{};
+
+void f0(); // shouldn't ever be matched, contains no parameters at all
+
+void f1(C c); // a C
+void f1(C c1, C c2);
+void f2(C& c); // lvalue-ref to a C
+void f3(const C& c); // lvalue-ref to a const C
+void f4(C&& c); // rvalue-ref to a C
+void f4_2(C&& c, C&& c2){
+    void f5(C&& c);
+};
+
+
+void frp1(C* c); // pointer to C
+void frp2(C*& c); // lvalue-ref to pointer to C
+void frp3(const C*& c); // lvalue-ref to pointer to const C
+void frp3_1(C* const & c); // lvalue-ref to constant pointer to C
+void frp3_2(const C* const & c); // lvalue-ref to constant pointer to constant C
+void frp4(C*&& c);
+
+
+void fp1(C* c);
+void fp2(const C* c); // pointer to a const C
+void fp3(C* const c); // const pointer to a C
+void fp4(const C* const c); // const pointer to a const C
