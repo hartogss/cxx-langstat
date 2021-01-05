6bab42bec788911f3dd77e5402b38f1fa0f1b727
hartogss <sigihart@bluewin.ch>
2021-01-05 17:17:38 +0100

    MSA added test & WIP is to get data for function templates

--- include/cxx-langstat/Analyses/MoveSemanticsAnalysis.h
+++ include/cxx-langstat/Analyses/MoveSemanticsAnalysis.h
@@ -16,8 +16,9 @@ public:
     void run(llvm::StringRef InFile, clang::ASTContext& Context) override;
 private:
     void extract();
-    template<typename T>
-    void gatherStatistics(std::string text, const Matches<T>& Matches);
+    void gatherStatistics(std::string text, const Matches<clang::ParmVarDecl>& Matches);
+    void gatherFunctionDeclData(std::string text,
+        const Matches<clang::FunctionDecl>& Matches);
     Matches<clang::FunctionDecl> FunctionDeclsWithCopy;
     Matches<clang::FunctionDecl> FunctionDeclsWithlValueRef;
     Matches<clang::FunctionDecl> FunctionDeclsWithConstlValueRef;
@@ -26,6 +27,10 @@ private:
     Matches<clang::ParmVarDecl> lValueRefParmDecls;
     Matches<clang::ParmVarDecl> ConstlValueRefParmDecls;
     Matches<clang::ParmVarDecl> rValueRefParmDecls;
+    Matches<clang::FunctionDecl> FunctionTemplatesDeclsWithCopy;
+    Matches<clang::FunctionDecl> FunctionTemplatesDeclsWithlValueRef;
+    Matches<clang::FunctionDecl> FunctionTemplatesDeclsWithConstlValueRef;
+    Matches<clang::FunctionDecl> FunctionTemplatesDeclsWithrValueRef;
 };
 
 #endif // MOVESEMANTICSANALYSIS_H
--- lib/Analyses/MoveSemanticsAnalysis.cpp
+++ lib/Analyses/MoveSemanticsAnalysis.cpp
@@ -13,57 +13,95 @@ using ordered_json = nlohmann::ordered_json;
 void MoveSemanticsAnalysis::extract(){
     // Writing the matcher like this means that a functionDecl can only bind to
     // one id - good, since it inhibits counting a decl twice under different ids
-    DeclarationMatcher functionMatcher = decl(anyOf(
-        functionDecl(unless(hasAnyParameter(hasType(referenceType()))))
-        .bind("copy"),
+    DeclarationMatcher functionMatcher = decl(isExpansionInMainFile(), anyOf(
+        functionDecl(hasAnyParameter(unless(hasType(referenceType()))))
+        .bind("value"), // problem: will also bind to func thats don't have any parameters
         functionDecl(hasAnyParameter(hasType(lValueReferenceType(
                 pointee(unless(isConstQualified()))))))
-        .bind("nonconstlvalue"),
+        .bind("nonconstlvalueref"),
         functionDecl(hasAnyParameter(hasType(lValueReferenceType(
                 pointee(isConstQualified())))))
-        .bind("constlvalue"),
+        .bind("constlvalueref"),
         functionDecl(hasAnyParameter(hasType(rValueReferenceType())))
-        .bind("rvalue")
+        .bind("rvalueref")
     ));
+    auto functions = Extractor.extract2(*Context, functionMatcher);
+    FunctionDeclsWithCopy = getASTNodes<FunctionDecl>(functions,
+        "value");
+    FunctionDeclsWithlValueRef = getASTNodes<FunctionDecl>(functions,
+        "nonconstlvalueref");
+    FunctionDeclsWithConstlValueRef = getASTNodes<FunctionDecl>(functions,
+        "constlvalueref");
+    FunctionDeclsWithrValueRef = getASTNodes<FunctionDecl>(functions,
+        "rvalueref");
 
-    DeclarationMatcher parmMatcher = decl(anyOf(
+    DeclarationMatcher parmMatcher = decl(isExpansionInMainFile(), anyOf(
         parmVarDecl(unless(hasType(referenceType())))
-        .bind("copy2"),
+        .bind("value"),
         parmVarDecl(hasType(lValueReferenceType(
                 pointee(unless(isConstQualified())))))
-        .bind("nonconstlvalue"),
+        .bind("nonconstlvalueref"),
         parmVarDecl(hasType(lValueReferenceType(
                 pointee(isConstQualified()))))
-        .bind("constlvalue"),
+        .bind("constlvalueref"),
         parmVarDecl(hasType(rValueReferenceType()))
-        .bind("rvalue")
+        .bind("rvalueref")
+    ));
+    auto parms = Extractor.extract2(*Context, parmMatcher);
+    CopyParmDecls = getASTNodes<ParmVarDecl>(parms, "value");
+    lValueRefParmDecls = getASTNodes<ParmVarDecl>(parms, "nonconstlvalueref");
+    ConstlValueRefParmDecls = getASTNodes<ParmVarDecl>(parms, "constlvalueref");
+    rValueRefParmDecls = getASTNodes<ParmVarDecl>(parms, "rvalueref");
+
+
+    DeclarationMatcher helper = decl(anyOf( // ameliorate with isExpansionInMainFile
+        functionDecl(hasAnyParameter(unless(hasType(referenceType()))))
+        .bind("value"), // problem: will also bind to func thats don't have any parameters
+        functionDecl(hasAnyParameter(hasType(lValueReferenceType(
+                pointee(unless(isConstQualified()))))))
+        .bind("nonconstlvalueref"),
+        functionDecl(hasAnyParameter(hasType(lValueReferenceType(
+                pointee(isConstQualified())))))
+        .bind("constlvalueref"),
+        functionDecl(hasAnyParameter(hasType(rValueReferenceType())))
+        .bind("rvalueref")
     ));
 
-    auto functions = Extractor.extract2(*Context, functionMatcher);
-    FunctionDeclsWithCopy = getASTNodes<FunctionDecl>(functions, "copy");
-    FunctionDeclsWithlValueRef = getASTNodes<FunctionDecl>(functions, "nonconstlvalue");
-    FunctionDeclsWithConstlValueRef = getASTNodes<FunctionDecl>(functions, "constlvalue");
-    FunctionDeclsWithrValueRef = getASTNodes<FunctionDecl>(functions, "rvalue");
-    auto parms = Extractor.extract2(*Context, parmMatcher);
-    CopyParmDecls = getASTNodes<ParmVarDecl>(parms,  "copy2");
-    lValueRefParmDecls = getASTNodes<ParmVarDecl>(parms, "nonconstlvalue");
-    ConstlValueRefParmDecls = getASTNodes<ParmVarDecl>(parms, "constlvalue");
-    rValueRefParmDecls = getASTNodes<ParmVarDecl>(parms, "rvalue");
+    DeclarationMatcher functionTemplateMatcher = functionTemplateDecl(
+        isExpansionInMainFile(),
+        has(helper
+    ));
+    auto functiontemplates = Extractor.extract2(*Context, functionTemplateMatcher);
+    FunctionTemplatesDeclsWithCopy = getASTNodes<FunctionDecl>(functiontemplates,
+        "value");
+    FunctionTemplatesDeclsWithlValueRef = getASTNodes<FunctionDecl>(functiontemplates,
+        "nonconstlvalueref");
+    FunctionTemplatesDeclsWithConstlValueRef = getASTNodes<FunctionDecl>(functiontemplates,
+        "constlvalueref");
+    FunctionTemplatesDeclsWithrValueRef = getASTNodes<FunctionDecl>(functiontemplates,
+        "rvalueref");
 }
 
-template<typename T>
-void MoveSemanticsAnalysis::gatherStatistics(std::string text,
-    const Matches<T>& Matches){
+void MoveSemanticsAnalysis::gatherStatistics(std::string PassKind,
+    const Matches<ParmVarDecl>& Matches){
         ordered_json Decls;
         for(auto match : Matches){
             ordered_json d;
             d["location"] = match.location;
-            // Has to be a vector, since there can be multiple function declarations
-            // or parameter variable declarations with the same name.
-            // Change when I switch to not idnexing JSON with decl name.
-            Decls[getMatchDeclName(match)].emplace_back(d);
+            Decls[PassKind][getMatchDeclName(match)].emplace_back(d);
         }
-        Result[text] = Decls;
+        Result["parm decls"].emplace_back(Decls);
+}
+
+void MoveSemanticsAnalysis::gatherFunctionDeclData(std::string PassKind,
+    const Matches<FunctionDecl>& Matches){
+        ordered_json Decls;
+        for(auto match : Matches){
+            ordered_json d;
+            d["location"] = match.location;
+            Decls[PassKind][getMatchDeclName(match)].emplace_back(d);
+        }
+        Result["function decls"].emplace_back(Decls);
 }
 
 void MoveSemanticsAnalysis::run(llvm::StringRef InFile,
@@ -71,14 +109,37 @@ void MoveSemanticsAnalysis::run(llvm::StringRef InFile,
         std::cout << "\033[32mRunning MSA:\033[0m" << std::endl;
         this->Context = &Context;
         extract();
-        gatherStatistics("fdecls with copying", FunctionDeclsWithCopy);
-        gatherStatistics("fdecls with non-const lvalue ref", FunctionDeclsWithlValueRef);
-        gatherStatistics("fdecls with const lvalue ref", FunctionDeclsWithConstlValueRef);
-        gatherStatistics("fdecls with rvalue ref", FunctionDeclsWithrValueRef);
-        gatherStatistics("copy parms", CopyParmDecls);
-        gatherStatistics("non-const lvalue ref parms", lValueRefParmDecls);
-        gatherStatistics("const lvalue ref parms", ConstlValueRefParmDecls);
-        gatherStatistics("rvalue parms", rValueRefParmDecls);
+        gatherFunctionDeclData("pass by value", FunctionDeclsWithCopy);
+        gatherFunctionDeclData("pass by non-const lvalue ref", FunctionDeclsWithlValueRef);
+        gatherFunctionDeclData("pass by const lvalue ref", FunctionDeclsWithConstlValueRef);
+        gatherFunctionDeclData("pass by rvalue ref", FunctionDeclsWithrValueRef);
+        //
+        gatherStatistics("value", CopyParmDecls);
+        gatherStatistics("non-const lvalue ref", lValueRefParmDecls);
+        gatherStatistics("const lvalue ref", ConstlValueRefParmDecls);
+        gatherStatistics("rvalue ref", rValueRefParmDecls);
+
+        // gatherFunctionDeclData("pass by value", FunctionTemplatesDeclsWithCopy);
+        // gatherFunctionDeclData("pass by non-const lvalue ref", FunctionTemplatesDeclsWithlValueRef);
+        // gatherFunctionDeclData("pass by const lvalue ref", FunctionTemplatesDeclsWithConstlValueRef);
+        // gatherFunctionDeclData("pass by rvalue ref", FunctionTemplatesDeclsWithrValueRef);
+
+        for(auto match : FunctionTemplatesDeclsWithrValueRef)
+            std::cout << match.node->getType().getAsString() << std::endl;
+
+
+        // std::cout << Result["rvalue ref parms"]["c"][0].dump(4) << std::endl;
+        // // how to avoid indexing using decl name?
+        // std::cout << Result["rvalue ref parms"].dump(4) << std::endl;
+        // // can also index using iterators:
+        // for(auto entriesForASingleVarName : Result["rvalue ref parms"]){
+        //     std::cout << varName << std::endl;
+        //     // for(auto loc : varName){
+        //         // std::cout << varName << "@ " << loc << std::endl;
+        //     // }
+        // }
+
+
 }
 
 //-----------------------------------------------------------------------------
