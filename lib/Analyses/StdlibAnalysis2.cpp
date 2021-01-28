90c2e3ef952c6132c455222cd8df401ce8ea0897
hartogss <sigihart@bluewin.ch>
2021-01-28 16:54:37 +0100

    SLA2: added function to compute statistics of type args used to instantiate stdlib templates, mostly interesting to get histogram of T for e.g. std::vector<T>

--- lib/Analyses/StdlibAnalysis2.cpp
+++ lib/Analyses/StdlibAnalysis2.cpp
@@ -22,13 +22,13 @@ using ordered_json = nlohmann::ordered_json;
 StdlibAnalysis2::StdlibAnalysis2() : TemplateInstantiationAnalysis(true,
     hasAnyName(
         // Standard library containers, Copied from UseAutoCheck.cpp
-        "std::array", "std::vector",
+        "std::array", "std::vector", "std::deque",
         "std::forward_list", "std::list",
-        "std::map", "std::multimap",
-        "std::set", "std::multiset",
-        "std::unordered_map", "std::unordered_multimap",
-        "std::unordered_set", "std::unordered_multiset",
-        "std::queue", "std::priority_queue", "std::stack", "std::deque",
+        "std::set", "std::map",
+        "std::multiset", "std::multimap",
+        "std::unordered_set", "std::unordered_map",
+        "std::unordered_multiset", "std::unordered_multimap",
+        "std::stack", "std::queue", "std::priority_queue",
         // Standard library utilities
         // vocabulary types
         "std::pair", "std::tuple",
@@ -39,29 +39,103 @@ StdlibAnalysis2::StdlibAnalysis2() : TemplateInstantiationAnalysis(true,
     std::cout << "SLA2 ctor\n";
 }
 
-ordered_json containerPrevalence(ordered_json Statistics, ordered_json j){
-    std::map<std::string, unsigned> m;
-    for(const auto& [containertype, containeroccurrences] : j["implicit class insts"].items()){
-        m.try_emplace(containertype, containeroccurrences.size());
+template<typename T>
+using StringMap = std::map<std::string, T>;
+
+// Map that for a stdlib type contains how many instantiation type args are
+// intersting to us, e.g. for the instantiation stored in a .json below,
+// "std::__1::vector" : {
+//     "location": "42"
+//     "arguments": {
+//         "non-type": [],
+//         "type": [
+//             "int",
+//             "std::__1::allocator<int>"
+//         ],
+//         "template": []
+//     }
+// }
+// the result is 1, since we only care about the fact that std::vector was
+// instantiated with int.
+// -1 indicates that we want all arguments interest us.
+// Used to analyze types contained in containers, utilities, smart pointer etc.
+const StringMap<int> NumRelTypes = { // no constexpr support for map
+    // ** Containers **
+    // sequential containers
+    {"std::array", 1}, {"std::vector", 1}, {"std::deque", 1},
+    {"std::forward_list", 1}, {"std::list", 1},
+    // associative containers
+    {"std::set", 1}, {"std::map", 2},
+    {"std::multiset", 1}, {"std::multimap", 2},
+    // Same, but unordered
+    {"std::unordered_set", 1}, {"std::unordered_map", 2},
+    {"std::unordered_multiset", 1}, {"std::unordered_multimap", 2},
+    // container adaptors
+    {"std::stack", 1}, {"std::queue", 1}, {"std::priority_queue", 1},
+    // ** Utilities **
+    // pairs and tuples
+    {"std::pair", 2}, {"std::tuple", -1},
+    // smart pointers
+    {"std::unique_ptr", 1}, {"std::shared_ptr", 1}, {"std::weak_ptr", 1}
+};
+
+// For some standard library type, get how many type arguments of instantiations
+// are interesting to us. Have to do some extra work in case it is std::__1-
+// qualified.
+int GetNumRelevantTypes(llvm::StringRef Type){
+    if(auto [l,r] = Type.split("::__1"); !r.empty()){
+        return NumRelTypes.at((l+r).str());
     }
-    std::string desc = "container type prevalences";
+    return NumRelTypes.at(Type.str());
+}
+
+// Actually gets the type arguments and concatenates them into a string.
+std::string GetRelevantTypesAsString(llvm::StringRef ContainerType,
+    nlohmann::json Types){
+        // std::cout << ContainerType.str() << std::endl;
+        int n = GetNumRelevantTypes(ContainerType);
+        if(n == -1)
+            n = Types.end()-Types.begin();
+        std::string t;
+        for(nlohmann::json::iterator i=Types.begin(); i<Types.begin()+n; i++)
+            t = t + (*i).get<std::string>() + ", ";
+        return llvm::StringRef(t).drop_back(2).str();
+}
+
+// Gathers data on how often standard library types were implicitly instantiated.
+ordered_json stdlibTypePrevalence(ordered_json Statistics, ordered_json j){
+    std::map<std::string, unsigned> m;
+    for(const auto& [Type, Insts] : j["implicit class insts"].items()){
+        m.try_emplace(Type, Insts.size());
+    }
+    std::string desc = "stdlib type prevalence";
     Statistics[desc] = m;
     return Statistics;
 }
 
-// WIP: find popular types for containers
-// ordered_json containedType(ordered_json Statistics, ordered_json j){
-//     std::map<std::string, std::map<std::string, unsigned>> m;
-//     for(const auto& [containertype, containeroccurrences] : j["implicit class insts"].items()){
-//         m.try_emplace(containertype, containeroccurrences.size());
-//     }
-//     std::string desc = "contained type prevalences";
-//     Statistics[desc] = m;
-//     return Statistics;
-// }
+// For standard library types, gathers data on what types
+// they were implicitly instantiated with.
+ordered_json stdlibInstantiationTypeArgs(ordered_json Statistics, ordered_json j){
+    StringMap<StringMap<unsigned>> m;
+    for(const auto& [Type, Insts] : j["implicit class insts"].items()){
+        for(const auto& Inst : Insts){
+            m.try_emplace(Type, StringMap<unsigned>());
+            nlohmann::json ContainedTypes = Inst["arguments"]["type"];
+            assert(ContainedTypes.is_array());
+            auto TypeString = GetRelevantTypesAsString(Type, ContainedTypes);
+            // std::cout << TypeString << std::endl;
+            m.at(Type).try_emplace(TypeString, 0);
+            m.at(Type).at(TypeString)++;
+        }
+    }
+    std::string desc = "stdlib instantiation type arguments";
+    Statistics[desc] = m;
+    return Statistics;
+}
 
 void StdlibAnalysis2::processFeatures(nlohmann::ordered_json j){
-    Statistics = containerPrevalence(Statistics, j);
+    Statistics = stdlibTypePrevalence(Statistics, j);
+    Statistics = stdlibInstantiationTypeArgs(Statistics, j);
 
 }
 
