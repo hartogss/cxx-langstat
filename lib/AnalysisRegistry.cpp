a4e43e86fa30fbaacde70ec7ad496f3586213aa5
hartogss <sigihart@bluewin.ch>
2021-02-15 21:37:33 +0100

    fixed s.t. supplying no --analyses=<string> means that all analyses are executed

--- lib/AnalysisRegistry.cpp
+++ lib/AnalysisRegistry.cpp
@@ -16,14 +16,25 @@ void AnalysisRegistry::createFreshAnalyses(){
     // was defined explicitly as a variable
     // Create all analyses and give them to the registry
 
-    // std::cout << ConstexprAnalysis::b << std::endl;
-    // AnalysisFactory::Create("cea");
-    for(const auto& ab : Options.EnabledAnalyses.Items){
-        auto analysis = AnalysisFactory::Create(ab.Name);
-        if(analysis)
-            Analyses.emplace_back(std::move(analysis));
-        else
-            std::cout << "Could not create analysis \"" << ab.Name << "\"\n";
+    // If some analyses are enabled
+    if(!Options.EnabledAnalyses.Items.empty()){
+        // std::cout << ConstexprAnalysis::b << std::endl;
+        // AnalysisFactory::Create("cea");
+        for(const auto& ab : Options.EnabledAnalyses.Items){
+            auto analysis = AnalysisFactory::Create(ab.Name);
+            if(analysis)
+                Analyses.emplace_back(std::move(analysis));
+            else
+                std::cout << "Could not create analysis \"" << ab.Name << "\"\n";
+        }
+    } else { // otherwise run all analyses
+        for(const auto& ab : AnalysisFactory::AnalyzerFactoryRegistry){
+            auto analysis = ab.second();
+            if(analysis)
+                Analyses.emplace_back(std::move(analysis));
+            else
+                std::cout << "Could not create analysis \"" << ab.first << "\"\n";
+        }
     }
 }
 
