ff082a9737aed0e74890bbd0dd28eccf283244fd
hartogss <sigihart@bluewin.ch>
2021-02-18 12:18:51 +0100

    CEA: added testfile illustrating problems with static member vars

---
+++ test/unit/constexpr/StaticMemberVar.cpp
@@ -0,0 +1,26 @@
+// RUN: rm %t1.ast.json || true
+// RUN: clang++ %s -emit-ast -o %t1.ast
+// RUN: %S/../../../build/cxx-langstat --analyses=cea -emit-features -in %t1.ast -out %t1.ast.json --
+// RUN: diff %t1.ast.json %s.json
+
+// Should report one variable that is constexpr, namely "yes".
+//
+//
+
+int main(int argc, char** argv){
+
+}
+
+struct C {
+    // Not reported, as not initialized.
+    static int no;
+    // Reported as not constexpr (good), but twice: once here, and once below
+    // at the definition. Do we want this?
+    static int no2;
+    // Not reported, as not init'ed.
+    static const int no3;
+    // Corectly reported as constexpr
+    static constexpr int yes = 1;
+};
+
+int C::no2 = 2;
